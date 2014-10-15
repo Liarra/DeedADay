00d29824f956d20aeea3cec8de7fcd48d013ec63
Nina Buchina <m.e.tigra@gmail.com>
2014-10-15 17:01:14 +0200

    oops, forgot models

---
+++ wsgi/openshift/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Hardness(models.Model):
+	name=models.CharField(max_length=20)
+	
+	def __unicode__(self):
+		 return self.name
+	
+class Tag(models.Model):
+	name=models.CharField(max_length=50)
+	
+	def __unicode__(self):
+		 return self.name
+
+class Deed(models.Model):
+	name=models.CharField(max_length=100)
+	description=models.CharField(max_length=500)
+	hardness=models.ForeignKey(Hardness)
+	tags=models.ManyToManyField(Tag)
+	image=models.ImageField(upload_to='deeds_img/')
+	
+	def __unicode__(self):
+		 return self.name
+
+class DeedUser(User):
+	deeds=models.ManyToManyField(Deed)
+	ext_id=models.CharField(max_length=50)
+	
