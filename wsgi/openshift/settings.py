de95b2a6292d658a39835efb2a5ea38bf42794ae
Nina Buchina <m.e.tigra@gmail.com>
2014-10-21 22:04:43 +0200

    Lala

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -82,8 +82,8 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
+#STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
