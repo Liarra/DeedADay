f39fb6e1a9b47dba6c3b0e51ce3d1d613d344c3f
Nina Buchina <m.e.tigra@gmail.com>
2014-10-30 14:11:28 +0100

    Remove obsolete URLs

--- wsgi/openshift/deedaday/urls.py
+++ wsgi/openshift/deedaday/urls.py
@@ -4,8 +4,6 @@ from deedaday import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),  
-    url(r'^$facebook/$', views.index_fb, name='index'),  
-    url(r'^$vk/$', views.index_vk, name='index'),  
     url(r'^deeds/(?P<deed_id>\d+)/$', views.deed, name='Deed detail'),   
     url(r'^user/$', views.userpage, name='User page'),   
     url(r'^send/$', views.sendADeed, name='Send your deed'),
